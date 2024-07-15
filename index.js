e840f16bf489e5fcb4fc9a83203cedb7658f816e
khadija-AC <khadijaaws21@gmail.com>
2024-07-15 20:00:12 +0100

    add index

---
+++ index.js
@@ -0,0 +1,16 @@
+(function (exports) {
+  function greet(name) {
+    const options = {
+      
+      month: 'long',
+      year: 'numeric',
+      weekday: 'numeric',
+    }
+
+    const now = new Date()
+    const formattedDate = now.toLocaleDateString('en-US', options)
+    return `Hello, ${name}! Today is ${formattedDate}.`
+  };
+
+  exports.greet = greet
+})(this)
